29a9ba64764f1af65665aaafdc74b3b1aef3ac2b
Daryllsaints <santoscdaryll@gmail.com>
2017-04-05 18:15:26 -0700

    Add files via upload

---
+++ Final_CarlosAndersonPhotography/index.js
@@ -0,0 +1,12 @@
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    }, 500);
+    return false;
+});
+
+$('.field').onClick(function(){
+    submitEvent.preventDefault()
+
+});
+
